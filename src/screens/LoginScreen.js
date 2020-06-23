176cf5435998817a8ae42f493e91b319899ccd6d
Ege <ege.yildir@hotmail.com>
2020-06-24 01:43:10 +0300

    Add login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Button } from 'react-native';
 
 function LoginScreen() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
     return (
         <div>
-            
+            <TextInput
+                placeholder="Email"
+                onChangeText={text => setEmail(text)}
+                //icon="email"
+                keyboardType="email-address"
+                //value={e-mail}
+            />
+            <TextInput
+                placeholder="Password"
+                onChangeText={text => setPassword(text)}
+                secureTextEntry
+                //value={password}
+            />
+            <Button
+                title="Login"
+                onPress={ () => {
+                    // TODO: Delete log statement after debugging.
+                    console.log(email + " " + password);
+                }}
+                color="#32E0C4"
+            />
+            <Button
+                title="Register"
+                onPress={ () => {
+                    console.log("goto Register...");
+                }}
+                color="#393E46"
+            />
         </div>
     )
 }
+
+const styles = StyleSheet.create({
+
+})
 
 export default LoginScreen
