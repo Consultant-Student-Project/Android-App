732b010bdf43f13bdb02788f7fb9a3715c189702
Mutlu Dönmez <mutlu.donmez@hotmail.com>
2020-07-02 02:18:19 +0300

    formik added to register & padding added

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
 import { Input } from "react-native-elements";
+import { Formik } from "formik";
 
 import colors from "../config/colors";
 
@@ -9,23 +10,46 @@ function RegisterScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <Input
-          placeholder="First name"
-          leftIcon={<FontAwesome5 name="user" />}
-        />
-        <Input
-          placeholder="Last name"
-          leftIcon={<FontAwesome5 name="user" />}
-        />
-        <Input
-          placeholder="Username"
-          leftIcon={<FontAwesome5 name="user-circle" />}
-        />
-        <Input
-          placeholder="E-mail"
-          leftIcon={<FontAwesome5 name="envelope" />}
-        />
-        <Input placeholder="Password" leftIcon={<FontAwesome5 name="lock" />} />
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ handleChange, handleSubmit }) => (
+            <>
+              <Input
+                placeholder="First name"
+                leftIcon={<FontAwesome5 name="user" />}
+              />
+              <Input
+                placeholder="Last name"
+                leftIcon={<FontAwesome5 name="user" />}
+              />
+              <Input
+                placeholder="Username"
+                autoCapitalize={false}
+                leftIcon={<FontAwesome5 name="user-circle" />}
+              />
+              <Input
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Email"
+                textContentType="emailAddress"
+                onChangeText={handleChange("email")}
+                leftIcon={<FontAwesome5 name="envelope" />}
+              />
+              <Input
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Password"
+                onChangeText={handleChange("password")}
+                textContentType="password"
+                secureTextEntry
+                leftIcon={<FontAwesome5 name="lock" />}
+              />
+              <Button title="Register" onPress={handleSubmit} color="#32e0c4" />
+            </>
+          )}
+        </Formik>
       </View>
     </View>
   );
@@ -33,13 +57,15 @@ function RegisterScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.background,
     flex: 1,
+    backgroundColor: colors.background,
+    flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
   },
   form: {
-    left: 20,
-    right: 20,
+    width: "100%",
+    paddingHorizontal: 20,
   },
 });
 export default RegisterScreen;
