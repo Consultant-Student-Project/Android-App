0eff0f76f0ef6ab6caf46b688ec053b431dcc0df
Ege <ege.yildir@hotmail.com>
2020-07-01 22:40:21 +0300

    Add profile picture

---
+++ src/components/ProfilePicture.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from "react-native";
+
+export default function ProfilePicture() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
