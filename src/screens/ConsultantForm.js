862d4b09dab0d3d8e7becb65d9556b26d1112193
Ege <ege.yildir@hotmail.com>
2020-07-02 11:13:50 +0300

    Add Consultant form

---
+++ src/screens/ConsultantForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+export default function ConsultantForm() {
+    return (
+        <View>
+            // TODO: Check database for needed info before making this form.
+        </View>
+    )
+}
