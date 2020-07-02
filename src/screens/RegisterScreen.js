2bbb33364276c4563e2a198211f1a4f4f1d4cc6c
Mutlu Dönmez <mutlu.donmez@hotmail.com>
2020-07-02 05:12:39 +0300

    yup, formik & input validation added

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,57 +1,92 @@
 import React from "react";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
-import { View, StyleSheet, Button } from "react-native";
+import { SafeAreaView, StyleSheet, Button } from "react-native";
 import { Input } from "react-native-elements";
 import { Formik } from "formik";
+import * as Yup from "yup";
 
 import colors from "../config/colors";
 
+const validationSchema = Yup.object().shape({
+  firstname: Yup.string().required().min(3).trim().label("First name"),
+  lastname: Yup.string().required().min(3).trim().label("Last name"),
+  username: Yup.string().required().min(3).trim().label("Username"),
+  email: Yup.string().required().email().trim().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
 function RegisterScreen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.form}>
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.form}>
         <Formik
-          initialValues={{ email: "", password: "" }}
+          initialValues={{
+            firstname: "",
+            lastname: "",
+            username: "",
+            email: "",
+            password: "",
+          }}
           onSubmit={(values) => console.log(values)}
+          validationSchema={validationSchema}
         >
-          {({ handleChange, handleSubmit }) => (
+          {({ handleChange, handleSubmit, errors, handleBlur, touched }) => (
             <>
               <Input
+                autoCapitalize="words"
                 placeholder="First name"
+                onBlur={handleBlur("firstname")}
+                onChangeText={handleChange("firstname")}
+                textContentType="name"
                 leftIcon={<FontAwesome5 name="user" />}
+                errorMessage={touched.firstname && errors.firstname}
               />
               <Input
+                autoCapitalize="words"
                 placeholder="Last name"
+                onBlur={handleBlur("lastname")}
+                onChangeText={handleChange("lastname")}
+                textContentType="familyName"
                 leftIcon={<FontAwesome5 name="user" />}
-              />
-              <Input
-                placeholder="Username"
-                autoCapitalize={false}
-                leftIcon={<FontAwesome5 name="user-circle" />}
+                errorMessage={touched.lastname && errors.lastname}
               />
               <Input
                 autoCapitalize="none"
                 autoCorrect={false}
+                onBlur={handleBlur("username")}
+                textContentType="username"
+                placeholder="Username"
+                onChangeText={handleChange("username")}
+                leftIcon={<FontAwesome5 name="user-circle" />}
+                errorMessage={touched.username && errors.username}
+              />
+              <Input
+                autoCapitalize="none"
+                autoCorrect={false}
+                onBlur={handleBlur("email")}
                 placeholder="Email"
                 textContentType="emailAddress"
                 onChangeText={handleChange("email")}
                 leftIcon={<FontAwesome5 name="envelope" />}
+                errorMessage={touched.email && errors.email}
               />
               <Input
                 autoCapitalize="none"
                 autoCorrect={false}
+                onBlur={handleBlur("password")}
                 placeholder="Password"
                 onChangeText={handleChange("password")}
                 textContentType="password"
                 secureTextEntry
                 leftIcon={<FontAwesome5 name="lock" />}
+                errorMessage={touched.password && errors.password}
               />
               <Button title="Register" onPress={handleSubmit} color="#32e0c4" />
             </>
           )}
         </Formik>
-      </View>
-    </View>
+      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
