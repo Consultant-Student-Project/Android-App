108089d527abc0d6fa891f9047f00875c0446e64
Mutlu Dönmez <mutlu.donmez@hotmail.com>
2020-07-03 22:33:45 +0300

    Working Chat Screen

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,56 +1,62 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
-import SocketIOClient from "socket.io-client";
-import { Button } from "react-native";
+// import SocketIOClient from "socket.io-client";
 
-export default class ChatScreen extends Component {
-  constructor(props) {
-    super(props);
+function ChatScreen() {
+  // constructor(props) {
+  //   super(props);
 
-    this.onSend = this.onSend.bind(this);
-    this.onReceivedMessage = this.onReceivedMessage.bind(this);
-    this.getUserId = this.getUserId.bind(this);
+  //   this.onSend = this.onSend.bind(this);
+  //   this.onReceivedMessage = this.onReceivedMessage.bind(this);
+  //   this.getUserId = this.getUserId.bind(this);
 
-    this.state = {
-      messages: [],
-      userId: null,
-      username: null,
-    };
+  //   this.state = {
+  //     messages: [],
+  //     userId: null,
+  //     username: null,
+  //   };
 
-    this.socket = SocketIOClient("http://localhost:3000");
-    this.socket.on("messages", this.onReceivedMessage);
-    this.socket.on("userId", this.getUserId);
-  }
+  //   this.socket = SocketIOClient("http://192.168.1.37:8080");
+  //   this.socket.on("messages", this.onReceivedMessage);
+  //   this.socket.on("userId", this.getUserId);
+  // }
 
-  getUserId(data) {
-    const { userId } = data;
-    this.setState({ userId });
-  }
+  // getUserId(data) {
+  //   const { userId } = data;
+  //   this.setState({ userId });
+  // }
 
-  onReceivedMessage(mes) {
-    const arrMes = [{ ...mes.messages }];
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, arrMes),
-    }));
-  }
-  onSend(messages) {
-    const mes = messages[0];
-    const { username } = this.state;
-    mes["username"] = username;
-    this.socket.emit("messages", mes);
-  }
-  render() {
-    return (
-      <>
-        <Button title="Connect" onPress={() => {}} />
-        <GiftedChat
-          messages={this.state.messages}
-          onSend={this.onSend}
-          user={{
-            _id: 1,
-          }}
-        />
-      </>
+  // onReceivedMessage(mes) {
+  //   const arrMes = [{ ...mes.messages }];
+  //   this.setState((previousState) => ({
+  //     messages: GiftedChat.append(previousState.messages, arrMes),
+  //   }));
+  // }
+  // onSend(messages) {
+  //   const mes = messages[0];
+  //   const { username } = this.state;
+  //   mes["username"] = username;
+  //   this.socket.emit("messages", mes);
+  // }
+  const [messages, setMessages] = useState([]);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
     );
-  }
+  }, []);
+
+  return (
+    <>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    </>
+  );
 }
+
+export default ChatScreen;
