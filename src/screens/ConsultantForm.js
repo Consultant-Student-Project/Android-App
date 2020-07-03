51463b535815a2132d87e31d13bbb80ca4f5fa9f
Ege <ege.yildir@hotmail.com>
2020-07-03 23:18:10 +0300

    Add photo picking feature to Consultant form

--- src/screens/ConsultantForm.js
+++ src/screens/ConsultantForm.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, Button, StyleSheet, ImagePickerIOS } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { Input, Button } from 'react-native-elements';
+import { Input } from 'react-native-elements';
+import * as ImagePicker from "expo-image-picker";
+
+import colors from "../config/colors";
 
 const validationSchema = Yup.object().shape({
     faculty: Yup.string().required().min(3).trim().label("Faculty"),
@@ -10,8 +13,30 @@ const validationSchema = Yup.object().shape({
 });
 
 export default function ConsultantForm() {
+const [imageUri, setImageUri] = useState();
+
+    const requestPermission = async () => {
+        const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+        if(!granted)
+        alert("Need permission");
+    }
+
+    useEffect(() => {
+        requestPermission();
+    }, [])
+
+    const selectImage= async () => {
+        try {
+            const result = await ImagePicker.launchImageLibraryAsync();
+            if(!result.cancelled)
+                setImageUri(result.uri);
+        } catch (error) {
+            console.log("Error");
+        }
+    }
+
     return (
-        <View>
+        <View style={styles.form}>
             <Formik 
                 initialValues={{
                     faculty: "",
@@ -26,11 +51,16 @@ export default function ConsultantForm() {
                             placeholder="Faculty"
                         />
                         <Input 
-                            placeholder="Deparment"
+                            placeholder="Department"
+                        />
+                        <Button 
+                            title="Select Image"
+                            onPress={selectImage}
                         />
                         <Button 
                             title="Become a Consultant!"
                             onPress={handleSubmit}
+                            color={colors.neon}
                         />
                     </>
                 )}
@@ -38,3 +68,9 @@ export default function ConsultantForm() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    form: {
+        padding: 10,
+    }
+})
