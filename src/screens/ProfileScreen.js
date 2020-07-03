f729bca7f5560a966090ea1b1ae211e7d58b0bbe
Ege <ege.yildir@hotmail.com>
2020-07-03 19:27:48 +0300

    Add logout button to Profile Screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet, Button } from "react-native";
 import { Text, Avatar } from "react-native-elements";
+import AuthContext from "../auth/context";
 
 import colors from '../config/colors';
 
@@ -10,6 +11,8 @@ function ProfileScreen({ navigation }) {
     const faculty = "Faculty of Engineering";
     const department = "Computer Engineering";
 
+    const { user, setUser } = useContext(AuthContext);
+
     return (
         <View style={styles.profile}>
             <Avatar
@@ -17,18 +20,29 @@ function ProfileScreen({ navigation }) {
                 rounded
                 source="https://picsum.photos/200/200"
             />
-            <Text h1>{name} </Text>
-            <Text h4>{username} </Text>
-            <Text h3>{faculty} </Text>
-            <Text h4>{department} </Text>
-
-            <View style={styles.button}>
+            <View style={styles.name}>
+                <Text h1>{name} </Text>
+                <Text h4>{username} </Text>
+            </View>
+            <View style={styles.faculty}>
+                <Text h3>{faculty} </Text>
+                <Text h4>{department} </Text>
+            </View>
+            <View style={styles.consButton}>
                 <Button
                     title="Become A Consultant!"
                     onPress={() => {
                         navigation.navigate("ConsultantForm");
                     }}
                     color={colors.neon}
+                />
+                
+            </View>
+            <View style={styles.logoutButton}>
+                <Button
+                    title="Logout"
+                    onPress={() => setUser(null)}
+                    color={colors.grey}
                 />
             </View>
             
@@ -41,8 +55,20 @@ const styles = StyleSheet.create({
         alignItems: "center",
         marginTop: 30,
     },
-    button: {
+    consButton: {
         marginTop: 50,
+        marginBottom: 10,
+    },
+    logoutButton: {
+        width: "50%",
+    },
+    name: {
+        marginTop: 10,
+        alignItems: "center",
+    },
+    faculty: {
+        marginTop: 25,
+        alignItems: "center",
     },
 })
 
